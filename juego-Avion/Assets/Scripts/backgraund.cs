f1c69f443aa173c0ce6a79edd33ddddd0a2c9834
jhossmar <jhosmar_ramos@hotmail.com>
2015-10-07 00:46:36 -0400

    poniento la camara, backgraund y el movimiento del backgraund

---
+++ juego-Avion/Assets/Scripts/backgraund.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgraund : MonoBehaviour {
+
+	public float velocidad =0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (0f, (Time.time*velocidad)); // hace que la textura se mueva en el eje y
+	}
+}
